e450df0118a8e53b60e35f73eda0b63d30ff663e
patrickdelumen <delumenpatrick@gmail.com>
2023-03-26 17:14:30 +0800

    Add responsive, button toggler and Order.js to change color if paid or pendding

---
+++ Admin Dashboard/JavaScript/Dashboard.js
@@ -0,0 +1,46 @@
+const sideMenu = document.querySelector("aside");
+const mainBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+
+console.log(sideMenu);
+
+mainBtn.addEventListener('click', () => { 
+    sideMenu.style.display = 'block';
+})
+
+closeBtn.addEventListener('click',  () => {
+    sideMenu.style.display = 'none';
+})
+
+// ==================THEME MODE==================
+
+const themeToggler = document.querySelector(".theme-toggler");
+console.log(themeToggler)
+
+themeToggler.addEventListener('click', () =>{
+    document.body.classList.toggle('dark-mode');
+
+    themeToggler.querySelector('i:nth-child(1)').classList.toggle('active')
+    themeToggler.querySelector('i:nth-child(2)').classList.toggle('active')
+
+})
+
+// ======================= ORDER IN TABLE==================
+
+orders.forEach(Order => {
+    const tr = document.createElement('tr');
+    const trContent =`
+                        <td>${Order.customerName}</td>
+                        <td>${Order.customerNumber}</td>
+                        <td>${Order.payment}</td>
+                        <td class="${Order.balance > 
+                        0 ? 'danger' : Order.
+                        balance === '0'? 'success'
+                        : 'primary'}">${Order.status}</td>
+                        <td class="primary">Details</td>
+                        `;
+                        console.log(orders)
+
+    tr.innerHTML = trContent;
+    document.querySelector('table tbody').appendChild(tr);
+})
---
+++ Admin Dashboard/JavaScript/Order.js
@@ -0,0 +1,32 @@
+const orders = [{
+    costumerName: 'Patz De Lumen',
+    customerNumber: '+639-124-906-866',
+    payment: 'Loan',
+    status: 'Pending',
+    balance: '300'
+},
+{
+    costumerName: 'John Doe',
+    customerNumber: '+639-704-906-453',
+    payment: 'Cash',
+    status: 'Paid',
+    balance: '0'
+},
+{
+    costumerName: 'Jomark Asul',
+    customerNumber: '+639-704-906-453',
+    payment: 'Cash',
+    status: 'Paid',
+    balance: '0'
+},
+{
+    costumerName: 'Jhie-Jhie',
+    customerNumber: '+639-704-906-453',
+    payment: 'Cash',
+    status: 'Paid',
+    balance: '0'
+}
+
+
+
+]
